cf89e958b735e7a4cc9a2bc1f392c9b225ad4cea
ariprasathsakthivel <ariprasath3@gmail.com>
2021-07-02 20:32:23 +0530

    added:chhange the current postion

--- Program.cs
+++ Program.cs
@@ -10,10 +10,23 @@ namespace snakeLadder
         }
         static void GameCheck()
         {
-            int startPoint = 0;
+            const int startPoint = 0;
+            int currentPosition = 0;
             Random random = new Random();
             int dieNumber = random.Next(1, 7);
-            Console.WriteLine("Die number is : {0}",dieNumber);
+            int option = random.Next(0, 3);
+            switch (option)
+            {
+                case 0 :
+                    break;
+                case 1 :
+                    currentPosition= currentPosition + dieNumber;
+                    break;
+                case 2 :
+                    currentPosition = currentPosition - dieNumber;
+                    break;
+            }
+            Console.WriteLine(currentPosition);
         }
     }
 }
