bc8c8f4da48ffe90b7bb4bcf485a725c2d587ef7
ariprasathsakthivel <ariprasath3@gmail.com>
2021-07-02 21:09:59 +0530

    added: ensure the player gets to exact winning postion

--- Program.cs
+++ Program.cs
@@ -12,11 +12,16 @@ namespace snakeLadder
         {
             const int startPoint = 0;
             int currentPosition = 0;
-            while (currentPosition <= 100)
+            while (currentPosition < 100)
             {
                 Random random = new Random();
                 int dieNumber = random.Next(1, 7);
                 int option = random.Next(0, 3);
+                Console.WriteLine(currentPosition);
+                if ((currentPosition+dieNumber) > 100)
+                {
+                    continue;
+                }
                 switch (option)
                 {
                     case 0:
