7ef99d3498fc79a03d646e971007aaa07e3cfd76
ariprasathsakthivel <ariprasath3@gmail.com>
2021-07-02 21:02:28 +0530

    updated: code to run till player reacher 100

--- Program.cs
+++ Program.cs
@@ -12,23 +12,26 @@ namespace snakeLadder
         {
             const int startPoint = 0;
             int currentPosition = 0;
-            Random random = new Random();
-            int dieNumber = random.Next(1, 7);
-            int option = random.Next(0, 3);
-            switch (option)
+            while (currentPosition <= 100)
             {
-                case 0 :
-                    break;
-                case 1 :
-                    currentPosition= currentPosition + dieNumber;
-                    break;
-                case 2 :
-                    currentPosition = currentPosition - dieNumber;
-                    break;
-            }
-            if (currentPosition <= 0)
-            {
-                currentPosition = startPoint;
+                Random random = new Random();
+                int dieNumber = random.Next(1, 7);
+                int option = random.Next(0, 3);
+                switch (option)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        currentPosition = currentPosition + dieNumber;
+                        break;
+                    case 2:
+                        currentPosition = currentPosition - dieNumber;
+                        break;
+                }
+                if (currentPosition <= 0)
+                {
+                    currentPosition = startPoint;
+                }
             }
             Console.WriteLine(currentPosition);
         }
