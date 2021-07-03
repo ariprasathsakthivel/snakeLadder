205586ebfaa77b0f4905c3e23c580664d9be5b98
ariprasathsakthivel <ariprasath3@gmail.com>
2021-07-03 09:08:18 +0530

    added: 2player game, results, comments

--- Program.cs
+++ Program.cs
@@ -6,41 +6,61 @@ namespace snakeLadder
     {
         static void Main(string[] args)
         {
-            GameCheck();
-        }
-        static void GameCheck()
-        {
-            const int startPoint = 0;
-            int currentPosition = 0;
+            int currentPosition1 = 0; //Position of Player1
+            int currentPosition2 = 0; //Position of Player2
             int gameCount = 0;
-            while (currentPosition <= 100)
+            while (currentPosition1 < 100 && currentPosition2 <100)
             {
                 gameCount++;
-                Random random = new Random();
-                int dieNumber = random.Next(1, 7);
-                int option = random.Next(0, 3);
-                Console.WriteLine(currentPosition);
-                if ((currentPosition+dieNumber) > 100 && currentPosition != 100)
-                {
-                    continue;
-                }
-                switch (option)
-                {
-                    case 0:
-                        break;
-                    case 1:
-                        currentPosition = currentPosition + dieNumber;
-                        break;
-                    case 2:
-                        currentPosition = currentPosition - dieNumber;
-                        break;
-                }
-                if (currentPosition <= 0)
-                {
-                    currentPosition = startPoint;
-                }
+                Console.WriteLine("The number of times the dice was played to win the game is {0}", gameCount);
+                currentPosition1 = GameCheck(currentPosition1);
+                Console.WriteLine("currentPosition of player1--> {0}", currentPosition1);
+                currentPosition2 = GameCheck(currentPosition2);
+                Console.WriteLine("currentPosition of player2--> {0}", currentPosition2);
             }
-            Console.WriteLine("The number of times the dice was played to win the game is {0}",gameCount);
+            if (currentPosition1 == 100 && currentPosition2 < 100)
+            {
+                Console.WriteLine("Player1 is the winner");
+                Console.WriteLine("Player2 lost the game");
+            }
+            else if (currentPosition1 == 100 && currentPosition2 == 100)
+            {
+                Console.WriteLine("The game is a tie");
+            }
+            else
+            {
+                Console.WriteLine("Player2 is the winner");
+                Console.WriteLine("Player1 lost the game");
+            }
+        }
+        static int GameCheck(int currentPosition)
+        {
+            const int startPoint = 0;
+            Random random = new Random();
+            int dieNumber = random.Next(1, 7); 
+            int option = random.Next(0, 3); 
+            if ((currentPosition + dieNumber) > 100)
+            {
+            }
+            else
+            {
+            switch (option)
+            {
+                case 0: //No play
+                    break;
+                case 1:  //Ladder
+                    currentPosition = currentPosition + dieNumber;
+                    break;
+                case 2:  //Snake
+                    currentPosition = currentPosition - dieNumber;
+                    break;
+            }
+            }
+            if (currentPosition <= 0)
+            {
+                currentPosition = startPoint;
+            }
+            return currentPosition;
         }
     }
 }
